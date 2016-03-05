959a9325bb4f5973adc024af33f6a7c53c3ee6d8
Theresa Palid <tpalid1@gmail.com>
2016-03-05 01:38:21 +0000

    Wikis controller and views

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,57 @@
+class WikisController < ApplicationController
+  def index
+   @wikis = Wiki.all
+  end
+
+  def show
+     @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+  
+  def create
+    @wiki = Wiki.new(wiki_params)
+    @wiki.user = current_user
+    if @wiki.save
+      flash[:notice] = "Wiki was saved!"
+      redirect_to [@wiki]
+    else
+      flash[:notice] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+  
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki was updated"
+      redirect_to [:wiki]
+    else
+      flash[:error] = "There was an error updating the wiki. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted."
+      render :index
+    else
+      flash[:notice] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+  
+  private
+  
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+end
