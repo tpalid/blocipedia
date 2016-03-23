d83ce6c98fd5cdeb9d2859bdd6af073cf2fce20f
Theresa Palid <tpalid1@gmail.com>
2016-03-23 15:19:15 +0000

    Progress on creating collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,21 +1,22 @@
 class CollaboratorsController < ApplicationController
     
     def index
-        @wiki = Wiki.find(params[:wiki_id])
+        @wiki = Wiki.friendly.find(params[:wiki_id])
         @collaborators = Collaborator.where(wiki_id: @wiki.id)
         @user_collaborators = User.where(id: @collaborators.pluck(:user_id))
         #need to find a way to delete users from the index- but I am iterating through a list of USERS in the index#view, not a list of collaborators. could I make this into a hash?
     end
     
     def new
-        @wiki = Wiki.find(params[:wiki_id])
+        @wiki = Wiki.friendly.find(params[:wiki_id])
         @collaborator = Collaborator.new
     end
     
     def create
         @user = User.find_by_email(params[:email])
-        #need to have a rescue in case user doesn't exist
-        @wiki = Wiki.find(params[:wiki_id])
+        @wiki = Wiki.friendly.find(params[:wiki_id])
+        
+        #to ensure user exists
         if @user
             @collaborator = Collaborator.create(
                 user_id: @user.id,
@@ -34,7 +35,7 @@ class CollaboratorsController < ApplicationController
     end
     
     def destroy
-        @wiki = Wiki.find(params[:id])
+        @wiki = Wiki.friendly.find(params[:id])
         @collaborator = Collaborator.find(params[:user_id])
         if @collaborator.destroy
             flash[:notice] = "The collaborator was deleted." #change this to make more sense!
@@ -45,7 +46,9 @@ class CollaboratorsController < ApplicationController
         end
     end
             
-            
+    private
+    
+    
     #strong params?
     #nest this in a way that I get information from wiki?- checking on blocit
 end
